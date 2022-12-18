f5795652663c2bef3dc5c99b60a72fb5ada477fa
Ефимов Максим <efimov_m@itstep.academy>
2022-12-18 17:51:45 +0300

    Добавьте файлы проекта.

---
+++ StlThreadsProject/StlThreadsProject.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void func1()
+{
+    for (int i = 0; i < 100; i++)
+    {
+        cout << "\tthread child " << i << "\n";
+        this_thread::sleep_for(chrono::milliseconds(100));
+    }
+}
+
+
+
+int main()
+{
+    thread th1(func1);
+
+    for (int i = 0; i < 100; i++)
+    {
+        cout << "thread main " << i << "\n";
+        this_thread::sleep_for(chrono::milliseconds(50));
+    }
+    th1.detach();
+}
