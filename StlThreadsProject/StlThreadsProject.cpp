dfab653abb167752a71c4bc2e57e359a5ffa74fe
Ефимов Максим <efimov_m@itstep.academy>
2022-12-25 14:26:19 +0300

    25-12-22

--- StlThreadsProject/StlThreadsProject.cpp
+++ StlThreadsProject/StlThreadsProject.cpp
@@ -1,14 +1,34 @@
 ﻿#include <iostream>
 #include <thread>
+#include <mutex>
+#include <future>
 
 using namespace std;
 
-void func1()
+
+mutex m;
+
+int func1()
 {
+    int c = 0;
     for (int i = 0; i < 100; i++)
     {
-        cout << "\tthread child " << i << "\n";
-        this_thread::sleep_for(chrono::milliseconds(100));
+        cout << "\t" << this_thread::get_id() << " thread " << i << "\n";
+        //this_thread::sleep_for(chrono::milliseconds(80));
+        c++;
+    }
+    return c;
+}
+
+void func2(int &c)
+{
+    for (int i = 0; i < 1000; i++)
+    {
+        cout << "\t" << this_thread::get_id() << " thread " << i << "\n";
+        //this_thread::sleep_for(chrono::milliseconds(80));
+        m.lock();
+        c++;
+        m.unlock();
     }
 }
 
@@ -16,12 +36,23 @@ void func1()
 
 int main()
 {
-    thread th1(func1);
+    int counter{};
+    //thread th1(func1, ref(counter));
+    //thread th2(func2, ref(counter));
+
+    auto res = async(func1);
 
     for (int i = 0; i < 100; i++)
     {
-        cout << "thread main " << i << "\n";
-        this_thread::sleep_for(chrono::milliseconds(50));
+        cout << this_thread::get_id() << " thread main " << i << "\n";
+        //this_thread::sleep_for(chrono::milliseconds(50));
     }
-    th1.detach();
+
+    res.wait();
+
+    cout << "=================== " << res.get() << "\n";
+    //th1.join();
+    //th2.join();
+
+    cout << counter << "\n";
 }
